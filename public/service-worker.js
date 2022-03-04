20be9093e80a30692b33328f33200bfe078b1852
BryceStockman <65779581+BryceStockman@users.noreply.github.com>
2022-03-03 18:03:38 -0700

    service-worker all icons added

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,10 +2,16 @@ const FILES_TO_CACHE = [
   './index.html',
   './css/styles.css',
   './icons/icon-72x72.png',
-  // add the rest
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png',
   './js/idb.js',
   './js/index.js',
-  // add the manifest.json
+  './manifest.json',
 ];
 
 const APP_PREFIX = 'BudgetTracker';
