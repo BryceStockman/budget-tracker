219418d725453097559ac57c812a38e3dac3e31b
BryceStockman <65779581+BryceStockman@users.noreply.github.com>
2022-03-03 18:01:23 -0700

    idb.js, service-worker.js, manifest.json added and setup

---
+++ public/js/idb.js
@@ -0,0 +1,60 @@
+const IndexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
+
+let DB;
+
+const request = IndexedDB.open('budget', 1);
+
+request.onupgradeneeded = ({ target }) => {
+  DB = target.result;
+  DB.createObjectStore('pending', { autoIncrement: true });
+};
+
+request.onsuccess = ({ target }) => {
+  DB = target.result;
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = ({ target }) => {
+  console.log(target.errorCode);
+};
+
+function saveTransaction(transaction) {
+  const moneyTransfer = DB.transaction(['pending'], 'readwrite');
+  const store = moneyTransfer.objectStore('pending');
+  store.add(transaction);
+}
+
+function checkDatabase() {
+  const moneyTransfer = DB.transaction(['pending'], 'readwrite');
+  const store = moneyTransfer.objectStore('pending');
+  const total = store.getAll();
+  total.onsuccess = function () {
+    if (total.result.length > 0) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(total.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((data) => {
+          return data.json();
+        })
+        .then(() => {
+          const moneyTransfer = DB.transaction(['pending'], 'readwrite');
+          const store = moneyTransfer.objectStore('pending');
+          store.clear();
+        });
+    }
+  };
+}
+
+window.addEventListener('online', checkDatabase);
